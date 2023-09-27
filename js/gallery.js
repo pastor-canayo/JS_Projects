c17b3d06940b0473f6bd3915d4c354db5afad6e7
Pastor Canayo <pastorelguero@hotmail.com>
2023-09-26 23:25:11 -0600

    added a new jsProject Background Color Changer

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,33 @@
-const btnEl = document.getElementById("btn");
+//const btnEl = document.getElementById("btn");
 
-function fetchImage(){
-// 	const inputValue = document.getElementById("input").value;
-// 	fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=xfLte0jsb7bep20dOLnWQQMXhbofJ4nt0FlSl5X9sko`).then((res)=>res.json().then((data)=>{
-// 		console.log(data);
-// 	})
-  // );
-	console.log("clciked");
+async function fetchImage(){
+	const inputValue = document.getElementById("input").value;
+	const errorMessageEl = document.getElementById("errorMessage");
+	const galleryEl = document.getElementById("gallery");
+	if(inputValue > 10 || inputValue < 1){
+		
+		errorMessageEl.style.display = "block";
+		return;
+	}
+	try{
+		await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random() * 1000)}&client_id=xfLte0jsb7bep20dOLnWQQMXhbofJ4nt0FlSl5X9sko`).then((res)=>res.json().then((data)=>{
+		     if(data){
+		   		data.forEach((pic)=>{
+		   			console.log(pic.url.small);
+		   		}) 
+		   }
+		})
+	  );
+		errorMessageEl.style.display = "none";
+	}
+	catch(error){
+		errorMessageEl.style.display = "block";
+		errorMessageEl.innerText = "An error happened, try again later";
+	}
+	
 }
-btnEl.addEventListener("click", fetchImage, false);
+window.onload=function(){
+  var btnEl = document.getElementById("btn");
+
+  btn.addEventListener("click", fetchImage, true);
+}
